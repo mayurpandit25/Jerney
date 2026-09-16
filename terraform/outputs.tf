67eb2fb9c96a1d3c1f2824812fd828de5f0ec5b5
Mayur Pandit <130488383+mayurpandit25@users.noreply.github.com>
2026-09-16 16:08:46 +0530

    Add outputs for private and public subnets
    Added outputs for private and public subnet IDs.

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -9,7 +9,7 @@ output "cluster_endpoint" {
 }
 
 output "cluster_certificate_authority" {
-  description = "EKS cluster CA certificate (base64)"
+  description = "EKS cluster CA certificate"
   value       = module.eks.cluster_certificate_authority_data
   sensitive   = true
 }
@@ -19,10 +19,17 @@ output "vpc_id" {
   value       = module.vpc.vpc_id
 }
 
+output "private_subnets" {
+  description = "Private subnet IDs"
+  value       = module.vpc.private_subnets
+}
+
+output "public_subnets" {
+  description = "Public subnet IDs"
+  value       = module.vpc.public_subnets
+}
+
 output "region" {
   description = "AWS region"
   value       = var.aws_region
 }
-
-# Use this command to configure kubectl after apply:
-# aws eks update-kubeconfig --region <region> --name <cluster_name>
