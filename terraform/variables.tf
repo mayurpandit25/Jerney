ca886bf0dee11c95a65d6f3804cf653c283bc5e5
Mayur Pandit <130488383+mayurpandit25@users.noreply.github.com>
2026-09-16 16:07:37 +0530

    Modify AWS region and variable descriptions
    Updated default values and descriptions for several variables.

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,17 +1,17 @@
 variable "aws_region" {
   description = "AWS region to deploy the EKS cluster"
   type        = string
-  default     = "us-east-1"
+  default     = "us-west-2"
 }
 
 variable "environment" {
-  description = "Environment name (dev, staging, prod)"
+  description = "Environment name"
   type        = string
   default     = "dev"
 }
 
 variable "cluster_name" {
-  description = "Name of the EKS cluster"
+  description = "EKS cluster name"
   type        = string
   default     = "jerney-eks"
 }
@@ -19,11 +19,11 @@ variable "cluster_name" {
 variable "cluster_version" {
   description = "Kubernetes version for EKS"
   type        = string
-  default     = "1.32"
+  default     = "1.35"
 }
 
 variable "vpc_cidr" {
-  description = "CIDR block for the VPC"
+  description = "CIDR block for VPC"
   type        = string
   default     = "10.0.0.0/16"
 }
